45204b7c4f7f179e49b22668f00adabbf0357714
Maosheng <chanmaosheng@gmail.com>
2020-04-18 13:39:52 +0800

    add context handler
    context handler is to register context callback in both acceptor and connection

--- handler.h
+++ handler.h
@@ -6,15 +6,28 @@
 namespace fantuan
 {
 class Connection;
-using OnConnection = std::function<void (const Connection&)>;
-using OnDisconnected = std::function<void (const Connection&)>;
-using OnData = std::function<void (const Connection&, uint16_t, char*)>;
+using OnConnection = std::function<void (Connection*)>;
+using OnDisconnected = std::function<void (Connection*)>;
+using OnData = std::function<void (Connection*, uint16_t, char*)>;
+using OnClose = std::function<void (Connection*)>;
 
 struct ConnectionHandler
 {
     OnConnection m_OnConnection;
     OnDisconnected m_OnDisconnected;
     OnData m_OnData;
+};
+
+class Context;
+using EventHandler = std::function<void()>;
+using UpdateContextHandler = std::function<void(Context*)>;
+struct ContextHandler
+{
+    EventHandler m_ReadHandler;
+    EventHandler m_WriteHandler;
+    EventHandler m_CloseHandler;
+    EventHandler m_ErrorHandler;
+    UpdateContextHandler m_UpdateContextHandler;
 };
 }
 
